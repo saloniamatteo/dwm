d4011a76d12dd4abfabda9a61f46136cfb3bee90
saloniamatteo <saloniamatteo@pm.me>
2024-11-28 16:19:27 +0100

    Update signals, remove capslock signal

--- dwm/config.h
+++ dwm/config.h
@@ -7,7 +7,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int swterminheritfs    = 1;        /* 1 terminal inherits fullscreen on unswallow, 0 otherwise */
 static unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
@@ -243,18 +243,18 @@ static const Key keys[] = {
 
 	/* XF* actions */
 	#include <X11/XF86keysym.h>
-	{ 0, XF86XK_AudioMute,         spawn,    SHCMD("wpctl set-mute @DEFAULT_SINK@ toggle; kill -44 $(pidof dwmblocks)") }, // Toggle volume
-	{ 0, XF86XK_AudioLowerVolume,  spawn,    SHCMD("wpctl set-volume @DEFAULT_SINK@ 5%-; kill -44 $(pidof dwmblocks)") },     // Decrease volume
-	{ 0, XF86XK_AudioRaiseVolume,  spawn,    SHCMD("wpctl set-volume @DEFAULT_SINK@ 5%+; kill -44 $(pidof dwmblocks)") },     // Increase volume
-	{ 0, XF86XK_AudioMicMute,      spawn,    SHCMD("wpctl set-mute @DEFAULT_SOURCE@ toggle; kill -44 $(pidof dwmblocks)") }, // Toggle mic
-	{ 0, XF86XK_MonBrightnessUp,   spawn,    SHCMD("xbacklight -inc 10; kill -45 $(pidof dwmblocks)") }, // Increase brightness
-	{ 0, XF86XK_MonBrightnessDown, spawn,    SHCMD("xbacklight -dec 10; kill -45 $(pidof dwmblocks)") }, // Decrease brightness
+	{ 0, XF86XK_AudioMute,         spawn,    SHCMD("wpctl set-mute @DEFAULT_SINK@ toggle; kill -40 $(pidof dwmblocks)") }, // Toggle volume
+	{ 0, XF86XK_AudioLowerVolume,  spawn,    SHCMD("wpctl set-volume @DEFAULT_SINK@ 5%-; kill -40 $(pidof dwmblocks)") },     // Decrease volume
+	{ 0, XF86XK_AudioRaiseVolume,  spawn,    SHCMD("wpctl set-volume @DEFAULT_SINK@ 5%+; kill -40 $(pidof dwmblocks)") },     // Increase volume
+	{ 0, XF86XK_AudioMicMute,      spawn,    SHCMD("wpctl set-mute @DEFAULT_SOURCE@ toggle; kill -40 $(pidof dwmblocks)") }, // Toggle mic
+	{ 0, XF86XK_MonBrightnessUp,   spawn,    SHCMD("xbacklight -inc 10; kill -37 $(pidof dwmblocks)") }, // Increase brightness
+	{ 0, XF86XK_MonBrightnessDown, spawn,    SHCMD("xbacklight -dec 10; kill -37 $(pidof dwmblocks)") }, // Decrease brightness
 	{ 0, XF86XK_TouchpadOn,        spawn,    SHCMD("synclient TouchpadOff=0") }, // Enable touchpad
 	{ 0, XF86XK_TouchpadOff,       spawn,    SHCMD("synclient TouchpadOff=1") }, // Disable touchpad
 	{ 0, XF86XK_TouchpadToggle,    spawn,    SHCMD("(synclient | grep 'TouchpadOff.*1' && synclient TouchpadOff=0) || synclient TouchpadOff=1") },
 
 	/* Others */
-	{ 0,      XK_Caps_Lock,  spawn,  SHCMD("kill -36 dwmblocks") }, // Update dwmblocks on CapsLock
+	//{ 0,      XK_Caps_Lock,  spawn,  SHCMD("kill -35 dwmblocks") }, // Update dwmblocks on CapsLock
 	{ 0,      XK_Print,      spawn,  SHCMD("scrot -q 100 -p \"$HOME/Pictures/Screenshot-%4Y%m%d_%H%M%S.png\"") },
 	{ MODKEY, XK_Print,	     spawn,  SHCMD("scrot -q 100 -s -p \"$HOME/Pictures/Screenshot-%4Y%m%d_%H%M%S.png\"") },
 
